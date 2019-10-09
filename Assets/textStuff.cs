d1dd914c471c57ff29eaaaffe818ac785d034f44
ajmods <ev3guy@gmail.com>
2019-10-09 14:50:59 -0500

    yeet

---
+++ Assets/textStuff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textStuff : MonoBehaviour
+{
+    public TextMeshProUGUI lat;
+
+    public TextMeshProUGUI lon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.location.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.location.isEnabledByUser)
+        {
+            lat.text = "Latitude: " + Input.location.lastData.latitude;
+            lon.text = "Longitude " + Input.location.lastData.longitude;
+        }
+    }
+}
