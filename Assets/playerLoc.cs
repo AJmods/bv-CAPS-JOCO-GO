e328df3082a94e970b3256b885d37cf782017681
ajmods <ev3guy@gmail.com>
2019-10-14 12:20:20 -0500

    added location tracking

---
+++ Assets/playerLoc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLoc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.location.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Input.location.lastData.latitude, transform.position.y, Input.location.lastData.longitude);
+    }
+}
