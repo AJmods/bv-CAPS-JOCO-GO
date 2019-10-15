597dccbe028f4c06a033c4b177e5d6d5cab5700d
adanda <ev3guy@gmail.com>
2019-10-15 12:27:33 -0500

    test scene done

--- Assets/playerLoc.cs
+++ Assets/playerLoc.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class playerLoc : MonoBehaviour
 {
+
+    public GameObject location_Mangager_object;
+    private LocationManager locationManager;
     // Start is called before the first frame update
     void Start()
     {
         Input.location.Start();
+        locationManager = location_Mangager_object.GetComponent<LocationManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Input.location.lastData.latitude, transform.position.y, Input.location.lastData.longitude);
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            transform.position = new Vector3(Input.location.lastData.latitude*locationManager.locationModifer, transform.position.y, Input.location.lastData.longitude * locationManager.locationModifer);
+        }
+        else
+        {
+            Input.location.Start();
+        }
     }
 }
