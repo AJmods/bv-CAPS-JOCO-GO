fdaa895221de2e1dbfb321c75ea87f77d39ac027
ajmods <ev3guy@gmail.com>
2019-10-30 14:39:51 -0500

    use new system to pop up menu

---
+++ Assets/Drews Stuff/SiteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiteController : MonoBehaviour
+{
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        menu.SetActive(true); //activate or deactivate the menu
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
